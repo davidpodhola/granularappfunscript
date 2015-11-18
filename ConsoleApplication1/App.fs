3095321208165a310676de0b9b2292487e895e26
davidpodhola <david.podhola@naseukoly.cz>
2015-11-18 22:17:47 +0100

    first attempt to attach to generated code

---
+++ ConsoleApplication1/App.fs
@@ -0,0 +1,27 @@
+﻿[<FunScript.JS>]
+module App
+
+open FunScript
+open FunScript.TypeScript
+
+// Allows writing jq?name for element access
+let jq(selector : string) = Globals.Dollar.Invoke selector
+let (?) jq name = jq("#" + name)
+
+[<JS; JSEmit("log.info({0})")>]
+let log_info (a) = failwith "never" |> ignore
+
+[<JS; JSEmit("log.enableAll()")>]
+let log_enable (b) = failwith "never" |> ignore
+
+[<JS; JSEmit("alert({0})")>]
+let hello (a) = failwith "never" |> ignore
+
+let main() = 
+    log_enable ()
+    log_info ( "application started" )
+    jq?MightyButton1.click(
+        fun _ -> 
+            log_info ( "button clicked" )
+            hello( "Clicked!" ) :> obj
+    ) |> ignore
---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,33 @@
+﻿[<EntryPoint>]
+let main argv = 
+    use appComposeHomeJSFile = System.IO.File.CreateText(@"..\Output.Web\Index.html")
+
+    fprintfn appComposeHomeJSFile """
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js" type="text/javascript"></script>
+    <script src="mscorlib.js" type="text/javascript"></script>
+    <script src="linq.js" type="text/javascript"></script>
+    <script src="Granular.Common.js" type="text/javascript"></script>
+    <script src="Granular.Presentation.js" type="text/javascript"></script>
+    <script src="Granular.Presentation.Generic.js" type="text/javascript"></script>
+    <script src="Granular.Host.js" type="text/javascript"></script>
+    <script src="GranularApplication1.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/loglevel/1.4.0/loglevel.min.js" type="text/javascript"></script>
+</head>
+<body>
+    <script type="text/javascript">
+        (function () {
+            'use strict';
+
+            $(document).ready(function () {
+                %s
+            });
+        })();
+    </script>
+</body>
+</html>
+    """ <| FunScript.Compiler.compileWithoutReturn <@ App.main() @>
+    0
